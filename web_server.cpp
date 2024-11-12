b9f37c1e7411beef3e8e7be2dd3dee7580d1d928
Lena <hladkovskaa@gmail.com>
2024-11-12 22:27:57 +0100

    merged

--- web_server.cpp
+++ web_server.cpp
@@ -15,6 +15,19 @@
 #include <fcntl.h>
 #include <sstream>
 #include <sys/epoll.h>
+
+/*
+Everything in C++ 98.
+execve, dup, dup2, pipe, strerror, gai_strerror,
+errno, dup, dup2, fork, socketpair, htons, htonl,
+ntohs, ntohl, select, poll, epoll (epoll_create,
+epoll_ctl, epoll_wait), kqueue (kqueue, kevent),
+socket, accept, listen, send, recv, chdir bind,
+connect, getaddrinfo, freeaddrinfo, setsockopt,
+getsockname, getprotobyname, fcntl, close, read,
+write, waitpid, kill, signal, access, stat, open,
+opendir, readdir and closedir.
+*/
 #include "Server.hpp"
 
 #define READ_BUFFER_SIZE 2048
