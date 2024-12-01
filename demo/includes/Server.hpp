d8a4c928666000e0f39118c27f60b674c70c097d
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-12-01 19:51:43 +0100

    feat: implemented members for Server class and default constructor

--- demo/includes/Server.hpp
+++ demo/includes/Server.hpp
@@ -1,8 +1,29 @@
-#include <iostream>
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <map>
+# include <string>
+# include <vector>
+# include <iostream>
 
 class Server
 {
-	Server();
-	~Server();
+	public:
+		Server();
+		// Server( Server const & src );
+		~Server();
+		// Server &		operator=( Server const & rhs );
 
+	private:
+		int									_location_nbr;
+		std::string							_host;
+		std::string							_port;
+		std::string							_root;
+		std::string							_access_log;
+		std::string							_client_max_body_size;
+		std::vector<std::string>			_indexes;
+		std::vector<std::string>			_server_names;
+		std::map<std::string, std::string>	_error_pages;
 };
+
+#endif /* ******************************************************** SERVER_HPP */
--- demo/srcs/Server.cpp
+++ demo/srcs/Server.cpp
@@ -1,6 +1,53 @@
 #include "../includes/Server.hpp"
 
-Server::Server() {}
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
 
-Server::~Server() {}
+Server::Server()
+{
+	this->_host = "localhost";
+	this->_root = "var/www/html";
+	this->_indexes.push_back("index.html");
+	this->_client_max_body_size = 1;
+}
 
+/* Server::Server( const Server & src )
+{
+} */
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Server::~Server()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+/* Server &				Server::operator=( Server const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+} */
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
