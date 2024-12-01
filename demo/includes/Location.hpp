8ad34e1f4db965203b73b134c1ee22ff8da2bbbc
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-12-01 19:51:22 +0100

    feat: implemented members for Location class and default constructor

--- demo/includes/Location.hpp
+++ demo/includes/Location.hpp
@@ -1,10 +1,38 @@
-#include <iostream>
+#ifndef LOCATION_HPP
+# define LOCATION_HPP
+
+# include <map>
+# include <string>
+# include <vector>
+# include <iostream>
 
 class Location
 {
-	Location();
-	~Location();
 
+	public:
+
+		Location();
+		// Location( Location const & src );
+		~Location();
+
+		// Location &		operator=( Location const & rhs );
+
+
+	private:
+
+		bool								_autoindex;
+		int									_timeout;
+		std::string							_client_max_body_size;
+		std::string							_name;
+		std::string							_root;
+		std::string							_upload_dir;
+		std::string							_cgi_extension;
+		std::vector<std::string>			_indexes;
+		std::vector<std::string>			_allowed_methods;
+		std::map<std::string, std::string>	_error_pages;
+		std::map<std::string, std::string>	_redir;
 };
 
-int parsing_config(std::string &configFilePath);
+// std::ostream &			operator<<( std::ostream & o, Location const & i );
+
+#endif /* ******************************************************** LOCATION_H */
--- demo/srcs/Location.cpp
+++ demo/srcs/Location.cpp
@@ -1,7 +1,53 @@
 #include "../includes/Location.hpp"
 
-Location::Location() {}
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
 
-Location::~Location() {}
+Location::Location()
+{
+	this->_root = "var/www/html";
+	this->_autoindex = false;
+	this->_indexes.push_back("index.html");
+	this->_client_max_body_size = 1;
+}
+
+/* Location::Location( const Location & src )
+{
+} */
 
 
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Location::~Location()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+/* Location &				Location::operator=( Location const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+} */
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
