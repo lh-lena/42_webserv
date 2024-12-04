46034e64e24dc65091611ea3bb7db8e6f04c45cf
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-12-04 21:02:37 +0100

    feat: Server: implementation set/get methods

--- demo/includes/Server.hpp
+++ demo/includes/Server.hpp
@@ -15,6 +15,27 @@ class Server
 		~Server();
 		// Server &		operator=( Server const & rhs );
 
+		void								setWorkCont(int connections);
+		void								setLocationNbr(int value);
+		void								setClientMaxBody(int value);
+		void								setHost(const std::string &arg);
+		void								setRoot(const std::string &arg);
+		void								setErrorLog(const std::string &arg);
+		void								setErrorPage(const std::string &key, const std::string &value);
+		void								setIndex(const std::string &arg);
+		void								setServerName(const std::string &arg);
+		void								setLocation(Location &src);
+		int									getWorkCont( void );
+		int									getLocationNbr( void );
+		int									getClientMaxBody( void );
+		std::string							getHost( void );
+		std::string							getRoot( void );
+		std::string							getErrorLog( void );
+		std::string							getErrorPage( const std::string &key, const std::string &value );
+		std::vector<std::string>			getIndex( void );
+		std::vector<std::string>			getServerName( void );
+		// std::vector<Location>			getLocation( void );
+
 	private:
 		int									_location_nbr;
 		int									_client_max_body_size;
--- demo/srcs/Server.cpp
+++ demo/srcs/Server.cpp
@@ -12,9 +12,11 @@ Server::Server()
 		_host("localhost"),
 		_port("8080"),
 		_root("./var/www/html"),
-		_error_log("error.log"),
-		_indexes({"index.html", "index.htm"})
-{}
+		_error_log("error.log")
+{
+	_indexes.push_back("index.html");
+	_indexes.push_back("index.htm");
+}
 
 /* Server::Server( const Server & src )
 {
@@ -48,10 +50,115 @@ Server::~Server()
 ** --------------------------------- METHODS ----------------------------------
 */
 
+void	Location::setRoot(const std::string &arg)
+{
+	_root = arg;
+}
+
+std::string	Location::getRoot( void )
+{
+	return (_root);
+}
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
+
+int Server::getWorkCont( void )
+{
+	return (_worker_connections);
+}
+
+int Server::getLocationNbr( void )
+{
+	return (_location_nbr);
+}
+
+int	Server::getClientMaxBody( void )
+{
+	return (_client_max_body_size);
+}
+
+std::string	Server::getHost( void )
+{
+	return (_host);
+}
+
+std::string	Server::getRoot( void )
+{
+	return (_root);
+}
+
+std::string	Server::getErrorLog( void )
+{
+	return (_error_log);
+}
+
+std::string	Server::getErrorPage( const std::string &key, const std::string &value )
+{
+	return (_error_pages[key]);
+}
+
+std::vector<std::string>	Server::getIndex( void )
+{
+	return (_indexes);
+}
+
+std::vector<std::string>	Server::getServerName( void )
+{
+	return (_server_names);
+}
+
+
+void Server::setWorkCont(int connections)
+{
+	_worker_connections = connections;
+}
+
+void Server::setLocationNbr(int value)
+{
+	_location_nbr = value;
+}
+
+void	Server::setClientMaxBody(int value)
+{
+	_client_max_body_size = value;
+}
+
+void	Server::setHost(const std::string &arg)
+{
+	_host = arg;
+}
+
+void	Server::setRoot(const std::string &arg)
+{
+	_root = arg;
+}
+
+void	Server::setErrorLog(const std::string &arg)
+{
+	_error_log = arg;
+}
+
+void	Server::setErrorPage(const std::string &key, const std::string &value)
+{
+	_error_pages[key] = value;
+}
+
+void	Server::setIndex(const std::string &arg)
+{
+	_indexes.push_back(arg);
+}
+
+void	Server::setServerName(const std::string &arg)
+{
+	_server_names.push_back(arg);
+}
+
+void	Server::setLocation(Location &src)
+{
+	_locations.push_back(src);
+}
 
 
 /* ************************************************************************** */
