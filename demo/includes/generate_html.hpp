e13aacbf7c7195dd2c215a5f80139f95c61d6073
Lena <hladkovskaa@gmail.com>
2025-01-09 22:19:36 +0100

    feat: generated an html error page

--- demo/includes/generate_html.hpp
+++ demo/includes/generate_html.hpp
@@ -5,7 +5,7 @@
 # include <vector>
 # include "utils.hpp"
 
-std::string generate_html_directory_listing(const std::string& dir_path);
-
+std::string		generate_html_directory_listing(const std::string& dir_path);
+std::string		generate_html_error_page(int status_code);
 
 #endif /** GENERATES_HTML_HPP */
