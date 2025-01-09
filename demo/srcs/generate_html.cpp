ebe220409fa477dbab75d46f0216dd6fd5e95497
Lena <hladkovskaa@gmail.com>
2025-01-09 22:18:37 +0100

    feat: generated an html error page

--- demo/srcs/generate_html.cpp
+++ demo/srcs/generate_html.cpp
@@ -37,3 +37,61 @@ std::string		generate_html_directory_listing(const std::string& dir_path)
 
 	return html.str();
 }
+
+std::string		generate_html_error_page(int status_code)
+{
+	std::stringstream	html;
+
+	html << "<!DOCTYPE html>\n"
+			"<html lang=\"en\">\n"
+			"<head>\n"
+			"    <meta charset=\"UTF-8\">\n"
+			"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+			"    <title>"
+			<< status_code
+			<< " "
+			<< get_reason_phrase(status_code)
+			<< "</title>\n"
+			"    <style>\n"
+			"        body {\n"
+			"            display: flex;\n"
+			"            justify-content: center;\n"
+			"            align-items: center;\n"
+			"            height: 100vh;\n"
+			"            margin: 0;\n"
+			"            font-family: Arial, sans-serif;\n"
+			"            background-color: #f2f2f2;\n"
+			"            color: #333;\n"
+			"        }\n"
+			"        .error-container {\n"
+			"            text-align: center;\n"
+			"            background: #fff;\n"
+			"            padding: 20px 40px;\n"
+			"            border: 1px solid #ddd;\n"
+			"            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);\n"
+			"            border-radius: 8px;\n"
+			"        }\n"
+			"        .error-container h1 {\n"
+			"            font-size: 3em;\n"
+			"            color: #e74c3c;\n"
+			"            margin: 0 0 20px;\n"
+			"        }\n"
+			"        .error-container p {\n"
+			"            font-size: 1.2em;\n"
+			"            margin: 0 0 15px;\n"
+			"        }\n"
+			"    </style>"
+			"</head>\n"
+			"<body>\n"
+			"    <div class=\"error-container\">"
+			"        <h1>"
+			<< get_reason_phrase(status_code)
+			<<"</h1>\n"
+			"        <p>"
+			<< get_status_message(status_code)
+			<< "</p>\n"
+			"</body>\n"
+			"</html>";
+
+	return html.str();
+}
