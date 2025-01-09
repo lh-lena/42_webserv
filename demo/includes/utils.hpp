e88810436609a06e59507249a75b8668a6ecf5bc
Lena <hladkovskaa@gmail.com>
2025-01-09 19:51:28 +0100

    additional functions imlementation

--- demo/includes/utils.hpp
+++ demo/includes/utils.hpp
@@ -1,6 +1,7 @@
 #ifndef UTILS_HPP
 # define UTILS_HPP
 
+# include <list>
 # include <string>
 # include <vector>
 # include <sstream>
@@ -8,7 +9,12 @@
 # include <cstring>
 # include <utility>
 # include <iostream>
+# include <dirent.h>
 # include <sys/stat.h>
+# include <sys/types.h>
+
+# define FOUND 1
+# define NOT_FOUND 0
 
 int							strToUint(std::string s);
 bool						is_status_code(int code);
@@ -16,10 +22,12 @@ bool						is_directory(const std::string& path);
 bool						is_regular_file(const std::string& path);
 bool						is_digits(const std::string& str);
 bool						is_path_exists(const std::string& path);
+bool						ends_with(const std::string& str, const std::string& suffix);
 std::string					intToStr(int i);
 std::string					reasonPhrase(int code);
 std::string					get_env_value(char **envp, const std::string &variable);
+std::string					generate_path(const std::string& base_path, const std::string& status_code);
 std::vector<std::string>	ft_split(std::string& s, std::string delimeter);
-std::string generate_path(const std::string& base_path, const std::string& status_code);
+int							get_dir_entries(const std::string& dirp, std::vector<std::string>& content);
 
 #endif /** UTILS_HPP */
