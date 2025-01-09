70e3a7d3f51581364007fbfb783b33dd90ce7fb6
Lena <hladkovskaa@gmail.com>
2025-01-09 19:52:05 +0100

    rm main function from ParseConfig

--- demo/srcs/ParseConfig.cpp
+++ demo/srcs/ParseConfig.cpp
@@ -613,25 +613,3 @@ ServerControler & ParseConfig::getServControler()
 
 /* ************************************************************************** */
 
-int main(int argc, char *argv[], char* envp[])
-{
-	(void)argc;
-	(void)argv;
-
-	std::string configFilePath;
-	configFilePath = "serv.conf";
-	ParseConfig config(configFilePath, envp);
-	try
-	{
-		config.readFileContent();
-		config.parseConfigContent();
-
-		ServerControler & controler = config.getServControler();
-		controler.startServing();
-	}
-	catch (std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return (1);
-	}
-}
