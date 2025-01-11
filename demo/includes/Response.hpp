d3700e82be21eba527e446613b5b910c379eddeb
Olena Hladkovska <ohladkov@student.42berlin.de>
2025-01-11 18:09:27 +0100

    Response struct created

---
+++ demo/includes/Response.hpp
@@ -0,0 +1,25 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+struct	Response
+{
+	uint		status_code;
+	uint		content_lenght;
+	bool		location_found;
+	std::string	path;
+	std::string	method;
+	std::string	protocol;
+	std::string	reason_phrase;
+	std::string	date;
+	std::string	content;
+	std::string	allow;
+	std::string	content_encoding; // ?
+	std::string	content_type;
+	std::string	src_location;
+};
+
+#endif /* RESPONSE_HPP */
