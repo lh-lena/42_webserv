8c6f33cc00544df49c22bce872c391cbc0bef97a
Olena Hladkovska <ohladkov@student.42berlin.de>
2025-01-11 18:10:07 +0100

    Request update

--- demo/includes/Request.hpp
+++ demo/includes/Request.hpp
@@ -1,14 +1,20 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
 #include <string>
 #include <vector>
 #include <map>
 
-enum Method {GET, POST, DELETE};
+enum Method {GET, POST, DELETE, UNSUPPORTED};
 
 struct Request
 {
 	Method		method;
+	std::string	method_r; /** added requested method as a string, for further checking */
 	std::string	host;
 	std::string	fileName;
 	std::string	reqBody;
 	char**		envVars;
 };
+
+#endif 
