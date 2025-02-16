3e7426ced1326d0122a2b41ba4a5bd9f08e68efe
Lena <hladkovskaa@gmail.com>
2025-02-16 11:11:57 +0100

    feat: CGI cookie test

---
+++ demo/var/cgi-bin/cookie_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import os, time
+
+# curl -v --cookie "Second=Fri, 16-Feb-2025 15:16:00; Count=3; InvalidCookie" http://localhost:8080/cookie_test.py
+
+VERSION = 0.1
+
+def get_cookie(_match):
+  # Returns the value from the matching cookie or '' if not defined.
+  if 'HTTP_COOKIE' in os.environ:
+    cookies = os.environ['HTTP_COOKIE']
+    cookies = cookies.split('; ')
+
+    for cookie in cookies:
+      if '=' in cookie:  # Ensure the cookie has a valid name=value structure.
+        (_name, _value) = cookie.split('=', 1)  # Split only on the first '='.
+        if _match.lower() == _name.lower():
+          return _value
+  return('')
+
+def val(_string):
+  # Returns the integer value of a string or zero if not numeric.
+  try:
+    return int(_string)
+  except:
+    return 0
+
+print ('Content-Type: text/html;charset=utf-8')
+if not get_cookie('first'): # Don't redefine value if aleady defined.
+  print ('Set-Cookie: First=' + time.asctime(time.localtime()))
+print ('Set-Cookie: Last=' + time.asctime(time.localtime()))
+print ('Set-Cookie: Count=' + str(val(get_cookie('Count')) + 1)) # Increment counter.
+print ()# End of header.
+
+print("<!DOCTYPE html>")
+print("<html lang=\"en\">")
+print("<head>")
+print("    <meta charset=\"UTF-8\">")
+print("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">")
+print("</head>")
+print ('<body><table border="0">')
+if get_cookie('first'): # Only dispay value if cookie is defined.
+  print ('<tr><td>Time First Visit</td><td>:</td><td>' + get_cookie('first') + '</tr>')
+if get_cookie('last'): # Only dispay value if cookie is defined.
+  print ('<tr><td>Time Last Visit</td><td>:</td><td>' + get_cookie('last') + '</tr>')
+print ('<tr><td>Counter</td><td>:</td><td>' + str(val(get_cookie('count'))) + '</tr>')
+print ('</table></body></html>')
