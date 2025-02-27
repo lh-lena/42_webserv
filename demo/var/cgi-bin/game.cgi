ee0cf6d32e2039d3c3e034ab27de969be668c578
Olena Hladkovska <ohladkov@student.42berlin.de>
2025-02-27 21:10:21 +0100

    chore: webpage in progress

--- demo/var/cgi-bin/game.cgi
+++ demo/var/cgi-bin/game.cgi
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-#nohup ./var/cgi-bin/2048 > /dev/null 2>&1 &
-# gnome-terminal -- /var/cgi-bin/2048 &
+echo "Working dir: $(pwd)"
 echo "Content-Type: text/html"
-echo "Location: /index.html"
+echo "Location: /main/index.html"
 echo "Status: 302 Found"
 echo ""
 
-osascript -e 'tell application "Terminal" to do script "Documents/webserv/demo/var/www/html/projects/2048"'
+#mac
+# osascript -e 'tell application "Terminal" to do script "Documents/webserv/demo/var/www/html/projects/2048_mac"'
+
+#linux
+# gnome-terminal -- bash -c "./var/www/html/projects/2048; exec bash"
+
+#nohup ./var/cgi-bin/2048 > /dev/null 2>&1 &
+
--- demo/var/www/html/file_create/script.js
+++ demo/var/www/html/file_create/script.js
@@ -1,10 +1,9 @@
 
-const fileInput = document.getElementById('file');
-const selectBtm = document.getElementById('uploadBtm');
-const fileNameDisplay = document.getElementById('fileName');
-const submitBtn = document.getElementById('submitBtn');
+const uploadBtm = document.getElementById('uploadBtm');
+const fileName = document.getElementById('fileName');
+const fileBtn = document.getElementById('fileBody');
 
-selectBtm.addEventListener('click', function () {
+uploadBtm.addEventListener('click', function () {
     fileInput.click();
 });
 
@@ -32,3 +31,38 @@ fileInput.addEventListener('change', function () {
 // }
 
 document.addEventListener("DOMContentLoaded", loadUploadedFiles);
+
+
+/* Processing request...
+request:
+method = POST
+reqURI = /file_create/index.html
+fullPath = 
+protocol = HTTP/1.1
+query = 
+upload path 
+body = ------WebKitFormBoundaryKVlZSrodmpq2r3vB
+Content-Disposition: form-data; name="file_name"
+
+fdf
+------WebKitFormBoundaryKVlZSrodmpq2r3vB
+Content-Disposition: form-data; name="file_body"
+
+fdfsdfsdf
+------WebKitFormBoundaryKVlZSrodmpq2r3vB--
+
+location: 
+	** Location **
+path: /
+root: 
+alias: 
+autoindex: 1
+clien max body: 0
+upload dir: 
+indexes: 
+CGI extention: 
+allowed methods: GET POST 
+error pages: 
+redirection: 
+
+ */
