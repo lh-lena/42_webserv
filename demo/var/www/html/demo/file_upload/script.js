21d6faa1dadacb1ae415471abbb01a294e2f360e
Olena Hladkovska <ohladkov@student.42berlin.de>
2025-03-02 17:35:26 +0100

    chore: file uploading in progress

--- demo/var/www/html/demo/file_upload/script.js
+++ demo/var/www/html/demo/file_upload/script.js
@@ -3,6 +3,7 @@ const fileInput = document.getElementById('file');
 const selectBtm = document.getElementById('uploadBtm');
 const fileNameDisplay = document.getElementById('fileName');
 const submitBtn = document.getElementById('submitBtn');
+const message = document.getElementById('message');
 
 selectBtm.addEventListener('click', function () {
     fileInput.click();
@@ -14,6 +15,21 @@ fileInput.addEventListener('change', function () {
     } else {
         fileNameDisplay.textContent = "No File Selected";
     }
+});
+
+submitBtn.addEventListener('click', function (e) {
+    if (fileInput.files.length === 0) {
+        e.preventDefault();
+        message.style.display = 'block';
+        message.innerHTML = "Please select a file before uploading";
+        setTimeout( () => {
+            message.style.display = 'none';
+            message.innerHTML = "";
+        }, 3000);
+    }
+    // else {
+    //     window.location.reload();
+    // }
 });
 
 // Fetch uploaded files and display them
